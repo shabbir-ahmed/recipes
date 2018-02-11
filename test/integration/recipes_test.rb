2fb83019a1ec485c30f2f78b347ea0648a63233e
Shabbir Ahmed <shabbir@salahsoftwaresolution.com>
2018-02-11 06:51:24 +0000

    Update

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -9,6 +9,15 @@ class RecipesTest < ActionDispatch::IntegrationTest
     
     test "create new valid recipe" do
         get new_recipe_path
+        # assert_template 'recipes/new'
+        # name_of_recipe = "Chicke salad"
+        # description_of_recipe = "Add chicken salad"
+        # assert_difference "Recipe.count", 1 do 
+        #     post recipes_path, params: {recipe: {name: name_of_recipe, description: description_of_recipe}}
+        # end
+        # follow_redirect!
+        # assert_match name_of_recipe.capitalize, response.body
+        # assert_match description_of_recipe, response.body
     end
     
     test "rejet invalid recipe submission" do 
@@ -18,8 +27,8 @@ class RecipesTest < ActionDispatch::IntegrationTest
             post recipes_path, params: {recipe: {name: "SASA", description: "SASA"}}
         end
         assert_template 'recipes/new'
-        # assert_select 'h2.panel-title'
-        # assert_select 'div.panel-body'
+        assert_select 'h2.panel-title'
+        assert_select 'div.panel-body'
     end
     
     
